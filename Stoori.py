10fc77ffbab4624f3da648dff51942e6db117196
CrazedCollie <163858278+CrazedCollie@users.noreply.github.com>
2024-12-11 14:10:21 +0000

    Firstup

--- Stoori.py
+++ Stoori.py
@@ -1,1 +1,33 @@
-Stoori tähän
+def Stoori():
+    print("Löydät itsesi istumasta vanhan metsämökin keittiöstä. Et ole varma kuinka tähän päädyttiin, mutta jostain tulee fiilis että poistuminen ripeästi saattaisi olla hyvä vaihtoehto.")
+
+    kumpi = input("Oletko Hannu vai Kerttu?")
+
+    print("Yrität katsoa josko oven saisi auki. Riittääkö voimia?(Kyllä/Ei)")
+	    If Kyllä:
+		    Vapaa, vapaa vihdoin! Mä en enää vandalisoi tätä kioskia!
+	    If Ei:
+		    Pitää etsiä toinen reitti.
+
+Ikkunat ovat lukossa. Otat pullaa. Niitä on kymmenisen kappaletta, tuoksuvatkin    		mokomat aika hyvälle. Tuoreita?
+Montako otat? (0-10)
+	If <5 AND Hannu = True
+		Pullat mukaan ja menoksi. Vapaa!
+	If 5>= OR Kerttu:
+		Ketuille meni, pitää jatkaa etsintöjä.
+
+Pullat on aika hyviä, mutta jumissa ollaan. Noita on krapuloissaan höpöttänyt lattialuukun reitistä, ehkä se toimii?
+Annatko hiirille pullaa että ehkä auttavat?
+	If Kyllä:
+		Reitti löytyy hiirten avulla. Vapaa!
+	If Ei:
+		Etsintä jatkuu.
+
+Noita saapastelee kotiin. Vaikuttaa olevan kännissä kuin käki.
+Kehutko leivontakykyjä?
+	If Kyllä:
+		Noita raivostuu ja päädyt itse uuniin seuraavan evään ainesosaksi. Bad end.
+	If Ei:
+		Noita hämmentyy kun ei löydä pulliaan. Työnnät uuniin kun täti tarkistaa sitä, ja
+		liukenet ovesta pihalle. Vapaa!
+
