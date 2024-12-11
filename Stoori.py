ef9d07291920622748df62dea6743b24797bc39b
CrazedCollie <163858278+CrazedCollie@users.noreply.github.com>
2024-12-11 14:55:28 +0000

    lisäälisää

--- Stoori.py
+++ Stoori.py
@@ -4,31 +4,30 @@ def Stoori():
     kumpi = input("Oletko Hannu vai Kerttu?")
 
     voima = input("Yrität katsoa josko oven saisi auki. Riittääkö voimia?(Kyllä/Ei)")
-	    If voima = Kyllä:
-		    print("Vapaa, vapaa vihdoin! Mä en enää vandalisoi tätä kioskia!")
-	    If voima = Ei:
-		    print("Pitää etsiä toinen reitti.")
+    If voima == "Kyllä":
+	    print("Vapaa, vapaa vihdoin! Mä en enää vandalisoi tätä kioskia!")
+    If voima == "Ei":
+	    print("Pitää etsiä toinen reitti.")
 
     print("Ikkunat ovat lukossa. Otat pullaa. Niitä on kymmenisen kappaletta, tuoksuvatkin mokomat aika hyvälle. Tuoreita?")
-    pullat = input("Montako otat? (0-10)")
-    If pullat <5 AND kumpi = Hannu:
+    pullat = int(input("Montako otat? (0-10)"))
+    If pullat <5 AND kumpi = "Hannu":
 	print("Pullat mukaan ja menoksi. Vapaa!")
-    break
-	If pullat 5>= OR kumpi = Kerttu:
+	If pullat 5>= OR kumpi = "Kerttu":
 		print("Ketuille meni, pitää jatkaa etsintöjä.")
 
     print("Pullat on aika hyviä, mutta jumissa ollaan. Noita on krapuloissaan höpöttänyt lattialuukun reitistä, ehkä se toimii?")
-Annatko hiirille pullaa että ehkä auttavat?
-	If Kyllä:
-		Reitti löytyy hiirten avulla. Vapaa!
-	If Ei:
-		Etsintä jatkuu.
+    hiiri = input("Annatko hiirille pullaa että ehkä auttavat?(Kyllä/Ei)")
+	If hiiri = "Kyllä":
+		print("Reitti löytyy hiirten avulla. Vapaa!")
+	If hiiri = "Ei":
+		print("Etsintä jatkuu.")
 
-Noita saapastelee kotiin. Vaikuttaa olevan kännissä kuin käki.
-Kehutko leivontakykyjä?
-	If Kyllä:
-		Noita raivostuu ja päädyt itse uuniin seuraavan evään ainesosaksi. Bad end.
-	If Ei:
-		Noita hämmentyy kun ei löydä pulliaan. Työnnät uuniin kun täti tarkistaa sitä, ja
-		liukenet ovesta pihalle. Vapaa!
+    print("Noita saapastelee kotiin. Vaikuttaa olevan kännissä kuin käki.")
+    kehu = input("Kehutko leivontakykyjä?(Kyllä/Ei)")
+	If kehu = "Kyllä":
+		print("Noita raivostuu ja päädyt itse uuniin seuraavan evään ainesosaksi. Bad end.")
+	If kehu = "Ei":
+		print("Noita hämmentyy kun ei löydä pulliaan. Työnnät uuniin kun täti tarkistaa sitä, ja liukenet ovesta pihalle. Vapaa!")
 
+Stoori()
